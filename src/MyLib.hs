4f5a32fc64d5d6a51ce3bff9c4fc8483b5fe71eb
Adam Matthews <>
2026-03-06 23:38:46 +0000

    implemented problem 3

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,10 +1,28 @@
-module MyLib (euler1, euler2) where
+module MyLib where
 
 fibs :: [Integer]
 fibs = 1 : 1 : zipWith (+) fibs (drop 1 fibs)
+
+primes :: [Integer]
+primes = filter isPrime [2 ..]
+
+isPrime :: Integer -> Bool
+isPrime 2 = True
+isPrime x
+  | x < 2 = False
+  | otherwise = null . primeFactors $ x
+
+primeFactors :: Integer -> [Integer]
+primeFactors x = underSqrt ++ overSqrt
+  where
+    underSqrt = filter (\p -> mod x p == 0) . takeWhile (\p -> p * p <= x) $ primes
+    overSqrt = filter isPrime . map (\p -> div x p) . reverse $ underSqrt
 
 euler1 :: Integer
 euler1 = sum . filter (\i -> mod i 3 == 0 || mod i 5 == 0) $ [1 .. 999]
 
 euler2 :: Integer
 euler2 = sum . filter even . takeWhile (<= 4000000) $ fibs
+
+euler3 :: Integer
+euler3 = last . primeFactors $ 600851475143
--- test/Main.hs
+++ test/Main.hs
@@ -10,4 +10,12 @@ main = hspec $ do
       shouldBe euler1 233168
     it "euler2" $ do
       shouldBe euler2 4613732
-
+    it "euler3" $ do
+      shouldBe euler3 6857
+  describe "primes" $ do
+    it "first 10" $ do
+      shouldBe (take 10 primes) [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+    it "1 is not prime" $ do
+      shouldBe (isPrime 1) False
+    it "prime factors of 34" $ do
+      shouldBe (primeFactors 34) [2, 17]
