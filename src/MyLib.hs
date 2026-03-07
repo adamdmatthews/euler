baffd2abf9c7446ae314d4dda26b7cb620b09157
Adam Matthews <>
2026-03-07 14:54:26 +0000

    implemented problem 5

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -23,6 +23,17 @@ isPalindrome x
   | x < 0 = False
   | otherwise = x == (read . reverse . show) x
 
+gcd' :: Integer -> Integer -> Integer
+gcd' a b = gcd'' (abs a) (abs b)
+  where
+    gcd'' x 0 = x
+    gcd'' x y = gcd'' y (mod x y)
+
+lcm' :: Integer -> Integer -> Integer
+lcm' 0 _ = 0
+lcm' _ 0 = 0
+lcm' x y = abs $ div (x * y) (gcd' x y)
+
 euler1 :: Integer
 euler1 = sum . filter (\i -> mod i 3 == 0 || mod i 5 == 0) $ [1 .. 999]
 
@@ -34,3 +45,6 @@ euler3 = last . primeFactors $ 600851475143
 
 euler4 :: Integer
 euler4 = maximum . filter isPalindrome $ [a * b | a <- [100 .. 999], b <- [100 .. a]]
+
+euler5 :: Integer
+euler5 = foldl1 lcm' [1 .. 20]
